24db97ffc533293ac90421f7e97aee43bc378c36
zhaorch <zhaorch@163.com>
2019-06-27 17:49:26 +0800

    view tutorial

--- MyProject/urls.py
+++ MyProject/urls.py
@@ -14,15 +14,34 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path,include,re_path
 from rest_framework import routers
 from school.views import GradeViewSet
+from school.view_tutorial import GradeListView, GradeListView2, GradeListView3, GradeListView4
+from school.view_tutorial import GradeListAPIView, GradeListAPIView2
+from school.view_tutorial import GradeGenericAPIView, GradeGenericAPIView2, GradeGenericAPIView3
+from school.view_tutorial import GradViewSetTemp, GradeGenericViewSet
 
 router = routers.DefaultRouter()
-router.register("grades",GradeViewSet)
+router.register("grades",GradeViewSet,"grades")
+router.register("study/grades0", GradViewSetTemp,"grades0")
+router.register("study/grades8", GradeGenericViewSet,"grades8")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('study/grades/', GradeListView.as_view()),
+    path('study/grades2/', GradeListView2.as_view()),
+    path('study/grades3/', GradeListView3.as_view()),
+    path('study/grades4/', GradeListView4.as_view()),
+
+    path('study/grades5/', GradeListAPIView.as_view()),
+    # re_path('^study/grades5/(?P<pk>[0-9]+)/$', GradeListAPIView2.as_view()),
+    path('study/grades5/<int:pk>/', GradeListAPIView2.as_view()),
+
+    path('study/grades6/', GradeGenericAPIView.as_view()),
+    path('study/grades7/', GradeGenericAPIView2.as_view()),
+    path('study/grades7/<int:pk>/', GradeGenericAPIView3.as_view())
 ]
---
+++ school/view_tutorial.py
@@ -0,0 +1,190 @@
+import json
+from django.views import View
+from django.http import HttpResponse, JsonResponse, Http404
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status, mixins, generics, viewsets
+from rest_framework.pagination import PageNumberPagination
+
+from .serializers import GradeSerializer
+from .models import Grade
+
+# -----------------------------------------------------------------
+
+
+class GradeListView(View):
+    def get(self,request):
+        grades = Grade.objects.all()[:5]
+
+        json_list = []
+        for grade in grades:
+            json_dict = dict()
+            json_dict['name'] = grade.name
+            json_dict['desc'] = grade.desc
+            # json_dict['created_time'] = good.created_time  # 日期没法序列化
+            json_list.append(json_dict)
+
+        return HttpResponse(json.dumps(json_list,ensure_ascii=False),content_type='application/json,charset=utf-8')
+
+
+class GradeListView2(View):
+    def get(self,request):
+        grades = Grade.objects.all()[:5]
+
+        from django.forms.models import model_to_dict
+        json_list = []
+        for grade in grades:
+            json_dict = model_to_dict(grade)  # ImageField 无法序列化
+            json_list.append(json_dict)
+
+        return HttpResponse(json.dumps(json_list,ensure_ascii=False),content_type='application/json,charset=utf-8')
+
+
+class GradeListView3(View):
+    def get(self,request):
+        grades = Grade.objects.all()[:5]
+
+        from django.core import serializers
+        json_data = serializers.serialize('json', grades, ensure_ascii=False)
+
+        return HttpResponse(json_data, content_type='application/json,charset=utf-8')
+
+
+class GradeListView4(View):
+    def get(self,request):
+        grades = Grade.objects.all()[:5]
+
+        from django.core import serializers
+        json_data = serializers.serialize('json', grades, ensure_ascii=False)
+        json_data = json.loads(json_data)
+        return JsonResponse(json_data, safe=False, json_dumps_params={'ensure_ascii' : False})
+
+# -----------------------------------------------------------------
+
+
+class GradeListAPIView(APIView):
+    def get(self, request, format=None):
+        grades = Grade.objects.all()[:5]
+        # serializer = GradeSerializer(instance=grades, many=True)
+        serializer = GradeSerializer(grades, many=True)
+        return Response(serializer.data)
+        # return JsonResponse(serializer.data, safe=False, json_dumps_params={'ensure_ascii' : False})
+
+    def post(self, request, format=None):
+        serializer = GradeSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class GradeListAPIView2(APIView):
+    def get_object(self, pk):
+        try:
+            return Grade.objects.get(pk=pk)
+        except Grade.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        grade = self.get_object(pk)
+        serializer = GradeSerializer(grade)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        grade = self.get_object(pk)
+        serializer = GradeSerializer(grade, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        grade = self.get_object(pk)
+        grade.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# -----------------------------------------------------------------
+
+
+class GoodsPagination(PageNumberPagination):
+    page_size = 5
+    page_size_query_param = 'page_size'
+    page_query_param = "page"
+    max_page_size = 100
+
+
+class GradeGenericAPIView(mixins.ListModelMixin, generics.GenericAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+    pagination_class = GoodsPagination
+
+
+    def get(self, request, *args, **kwargs) :
+        return self.list(request, *args, **kwargs)
+
+
+class GradeGenericAPIView2(generics.ListAPIView, generics.CreateAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+    pagination_class = GoodsPagination
+
+
+class GradeGenericAPIView3(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+
+# -----------------------------------------------------------------
+
+
+class GradViewSetTemp(viewsets.ViewSet):
+    def list(self, request):
+        queryset = Grade.objects.all()
+        serializer = GradeSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def create(self, request):
+        serializer = GradeSerializer(data=request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def retrieve(self, request, pk=None):
+        grade = Grade.objects.get(pk=pk)
+        serializer = GradeSerializer(grade)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        grade = self.get_object(pk)
+        serializer = GradeSerializer(grade, data=request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def partial_update(self, request, pk=None):
+        grade = self.get_object(pk)
+        serializer = GradeSerializer(grade, data=request.data)
+        if serializer.is_valid() :
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def destroy(self, request, pk=None):
+        grade = self.get_object(pk)
+        grade.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+# -----------------------------------------------------------------
+
+
+# class GradeGenericViewSet(viewsets.ModelViewSet): 可简写为ModelViewSet,继承了增删改查的所有方法
+class GradeGenericViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,mixins.CreateModelMixin,
+                          mixins.UpdateModelMixin,mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    queryset = Grade.objects.all()
+    serializer_class = GradeSerializer
+    pagination_class = GoodsPagination
+
+
+
